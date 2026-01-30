6957275536277afb901cf05675a360a88cb03f6f
Wassimchalak <wassimchalak05@gmail.com>
2026-01-30 22:36:27 +0200

    Add health endpoint and allow public access

---
+++ src/main/java/com/eduassist/eduassist/EduassistApplication.java
@@ -0,0 +1,13 @@
+package com.eduassist.eduassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduassistApplication.class, args);
+	}
+
+}
